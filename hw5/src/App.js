71ad618dc4e7e7edc209960fec2fbf79f379f14f
haobohong <san2664@gmail.com>
2020-12-07 22:08:25 +0800

    hw5

---
+++ hw5/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './login';
+
+function App() {
+  return (
+   <LoginForm />
+  );  
+}
+
+export default App;
---
+++ hw5_login/routes/user-routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const express = require("express");
+const usersController = require("../controllers/users-controllers");
+
+const router = express.Router();
+
+router.post("/signup", usersController.signup);
+router.post("/login", usersController.login);
+
+module.exports = router;
